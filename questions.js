946f0168c46702fb662a8cbbddab62df2b67e142
Teresa Liu <kuas033@gmail.com>
2019-12-14 22:00:31 -0800

    simple html css and js

---
+++ questions.js
@@ -0,0 +1,47 @@
+var questions = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+
+        choiceA: "<javascript>",
+        choiceB: "<script>",
+        choiceC: "<scripting>",
+        choiceD: "<js>"
+        correct: "B"
+    },
+
+    {
+        question: "Where is the correct place to insert a JavaScript?",
+
+        choiceA: "Both the <head> and <body> section are correct",
+        choiceB: "The <body> section",
+        choiceC: "The <head> section"
+        correct: "A"
+
+    },
+    {
+        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+
+        choiceA: "<script src = 'xxx.js'>",
+        choiceB: "<script href = 'xxx.js'>",
+        choiceC: "<script name='xxx.js'>"
+        correct: "A"
+
+    },
+    {
+        question: "The external JavaScript file must contain the <script> tag",
+        
+        choiceA: "True",
+        choiceB: "False"
+        correct: "B"
+
+        },
+    {
+        question: "How do you create a function in JavaScript?",
+        choiceA: "Function =myFunction()",
+        choiceB: "Function myFunction()",
+        choiceC: "Function:myFunction()"
+        correct: "B"
+
+        }
+
+]
